2c20eb69471e33844c295e254e73b141c2c102ca
Daniel Rojas <l20j45_@gmail.com>
2024-12-19 10:24:41 -0600

    Termine el post y el Get

--- db/seed.ts
+++ db/seed.ts
@@ -1,7 +1,14 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+	await db.insert(Clients).values([
+		{ id: 1, name: 'raul', age: 30, isActive: true },
+		{ id: 2, name: 'manuel', age: 30, isActive: true },
+		{ id: 3, name: 'alicia', age: 30, isActive: true },
+		{ id: 4, name: 'saul', age: 30, isActive: true },
+
+	]);
 	console.log('db seeded');
 }
--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,10 +1,12 @@
 import type { APIRoute } from 'astro';
+import { Clients, db } from 'astro:db';
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ }) => {
+    const resp = await db.select().from(Clients);
 
-    return new Response(JSON.stringify({ method: `Get` }), {
+    return new Response(JSON.stringify({ method: `Get`, ...resp }), {
         status: 200,
         headers: {
             'Content-Type': 'application/json',
@@ -12,17 +14,34 @@ export const GET: APIRoute = async ({ }) => {
     });
 };
 
-export const POST: APIRoute = async ({  }) => {
-    return new Response(
-        JSON.stringify({
-            method: 'POST',
+export const POST: APIRoute = async ({ params, request }) => {
 
-        }),
-        {
-            status: 200,
+    try {
+        const { id, ...body } = await request.json();
+
+        const resp = await db.insert(Clients).values(body);
+        console.log("🚀 ~ constPOST:APIRoute= ~ resp:", resp)
+
+        return new Response(
+            JSON.stringify({
+                id: resp.lastInsertRowid?.toString(),
+                ...body,
+            }),
+            {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+    }
+    catch (e) {
+        return new Response(JSON.stringify({ msg: e.message }), {
+            status: 400,
             headers: {
                 'Content-Type': 'application/json',
             },
-        }
-    );
+        });
+    }
+
 };
